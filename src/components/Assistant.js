67e61c0e5e79a40325d060494accd9bdfd1ed829
Adam Johnston <adam.johnston3679@gmail.com>
2017-12-19 19:30:48 +0000

    initial version of the assistant parent component with working state and props passed to future Switch component

---
+++ src/components/Assistant.js
@@ -0,0 +1,41 @@
+import React, { Component, Children, cloneElement } from 'react'
+import PropTypes from 'prop-types'
+
+export default class Assistant extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isListening: false
+    }
+  }
+
+  toggleListening = () => {
+    this.setState(state => {
+      return {
+        isListening: !state.isListening
+      }
+    })
+  }
+
+  render() {
+    const isListening = this.state.isListening
+    const children = this.props.children
+
+    return Children.map(children, child => {
+      if (child.type.name === 'Switch')
+        return cloneElement(child, {
+          isListening,
+          clickHandler: this.toggleListening
+        })
+
+      return child
+    })
+  }
+}
+
+Assistant.propTypes = {
+  actions: PropTypes.arrayOf(
+    PropTypes.object.isRequired
+  ).isRequired
+}
